b7b925d5d4f01a4ef5fac35bf13e39f633ba8dd8
Soulaimane02 <zelloufi.soulaimane@gmail.com>
2024-12-05 19:56:58 +0100

    add blocker scrapper

--- ReCaptcha.py
+++ ReCaptcha.py
@@ -1,1 +1,26 @@
-print("test")
+from tkinter import *
+from flask import Flask, request, abort
+
+app = Flask(__name__)
+
+@app.before_request
+def block_scrapers():
+    user_agent = request.headers.get('User-Agent')
+    banned_agents = ['Scrapy', 'PostmanRuntime', 'python-requests', 'Selenium']
+    if any(bot in user_agent for bot in banned_agents):
+        abort(403)
+
+
+@app.route("/")
+def home():
+    fenetre = Tk()
+    fenetre.geometry('400x400')
+    fenetre.title('Captcha-python')
+    fenetre['bg'] = 'grey'
+    fenetre.resizable(height=False, width=False)
+
+    fenetre.mainloop()
+
+
+if __name__ == "__main__":
+    home()
